3c08e72902531dd12452d6f2427f283a45d22df9
Guillermo Ruiz Álvarez <guillermo.ruiz.alv@gmail.com>
2019-07-08 23:08:01 +0100

    Add wavelet

---
+++ experiments/scripts/r_wavelet.m
@@ -0,0 +1,24 @@
+% Reduce noise using imbilatfilt
+function [] = r_wiener(input_path, output_path)
+
+    % Create output dir
+    disp("Creating " + output_path);
+    mkdir(output_path);
+    
+    files = dir(fullfile(input_path,'*'));
+    for i = 1:length(files)
+        in_file_name = files(i).name;
+        in_abs_path = fullfile(input_path, in_file_name);
+	% Process only files
+	if isfile(in_abs_path)
+        disp("Processing " + in_file_name)
+        in_image = imread(in_abs_path);
+	    % Apply filter
+        out_image = uint8(wdenoise2(in_image));
+        imwrite(out_image, fullfile(output_path, in_file_name));
+	else
+        disp("Skipping " + in_file_name)
+	end
+    end
+
+end
