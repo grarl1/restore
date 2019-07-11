3951f2a04a3eaa22a6e28eca69bdba64df9c069e
Guillermo Ruiz Álvarez <guillermo.ruiz.alv@gmail.com>
2019-07-11 21:05:54 +0100

    Finish first scripts for experiments

--- experiments/scripts/apply_fsrcnn.sh
+++ experiments/scripts/apply_fsrcnn.sh
@@ -4,11 +4,11 @@ cd /home/rual/workspace/master/tfm/restore/CNN_Keras
 # Set net
 unlink checkpoint
 unlink config.ini
-ln -s saved_checkpoints/IRCNN/checkpoint
-ln -s saved_checkpoints/IRCNN/config.ini
+ln -s saved_checkpoints/FSRCNN/checkpoint
+ln -s saved_checkpoints/FSRCNN/config.ini
 
-INPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise
-OUTPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn
+INPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn
+OUTPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn_fsrcnn
 mkdir -p $OUTPUT_DIR
 
 ls $INPUT_DIR | xargs -I % sh -c "python main.py -i $INPUT_DIR/% -o $OUTPUT_DIR/%"
