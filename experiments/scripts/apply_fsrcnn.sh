5dd6073929faab5a255144d29daef979c9b632ad
Guillermo Ruiz Álvarez <guillermo.ruiz.alv@gmail.com>
2019-07-11 21:45:24 +0100

    Generalize to take input path as an argument

--- experiments/scripts/apply_fsrcnn.sh
+++ experiments/scripts/apply_fsrcnn.sh
@@ -1,3 +1,15 @@
+# Check arguments
+if [ $# != 1 ]
+then
+    echo "Usage: $0 input_dir"
+    exit 1
+fi
+
+# Process arguments
+INPUT_DIR=`readlink -f $1`
+OUTPUT_DIR=${INPUT_DIR}_fsrcnn
+mkdir -p $OUTPUT_DIR
+
 # Go to repo
 cd /home/rual/workspace/master/tfm/restore/CNN_Keras
 
@@ -7,10 +19,7 @@ unlink config.ini
 ln -s saved_checkpoints/FSRCNN/checkpoint
 ln -s saved_checkpoints/FSRCNN/config.ini
 
-INPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn
-OUTPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn_fsrcnn
-mkdir -p $OUTPUT_DIR
-
+# Apply
 ls $INPUT_DIR | xargs -I % sh -c "python main.py -i $INPUT_DIR/% -o $OUTPUT_DIR/%"
 
 # Clean
--- experiments/scripts/apply_ircnn.sh
+++ experiments/scripts/apply_ircnn.sh
@@ -1,3 +1,15 @@
+# Check arguments
+if [ $# != 1 ]
+then
+    echo "Usage: $0 input_dir"
+    exit 1
+fi
+
+# Process arguments
+INPUT_DIR=`readlink -f $1`
+OUTPUT_DIR=${INPUT_DIR}_ircnn
+mkdir -p $OUTPUT_DIR
+
 # Go to repo
 cd /home/rual/workspace/master/tfm/restore/CNN_Keras
 
@@ -7,10 +19,7 @@ unlink config.ini
 ln -s saved_checkpoints/IRCNN/checkpoint
 ln -s saved_checkpoints/IRCNN/config.ini
 
-INPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise
-OUTPUT_DIR=/home/rual/workspace/master/tfm/restore/experiments/output/downscale_noise_ircnn
-mkdir -p $OUTPUT_DIR
-
+# Apply
 ls $INPUT_DIR | xargs -I % sh -c "python main.py -i $INPUT_DIR/% -o $OUTPUT_DIR/%"
 
 # Clean
